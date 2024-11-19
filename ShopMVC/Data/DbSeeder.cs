be9cca38b0b8ca103631436937aff1bb3b3b8a9d
fffvortex <fvortex@mail.ru>
2024-11-19 16:27:04 +0300

    add roles

---
+++ ShopMVC/Data/DbSeeder.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ShopMVC.Constants;
+
+namespace ShopMVC.Data
+{
+    public class DbSeeder
+    {
+        public static async Task SeedDefaultSeeder(IServiceProvider service)
+        {
+            var userManager = service.GetService<UserManager<IdentityUser>>();
+            var roleManager = service.GetService<RoleManager<IdentityRole>>();
+
+            // adding some rules
+
+            await roleManager.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Roles.User.ToString()));
+
+            // create admin
+
+            var admin = new IdentityUser
+            {
+                UserName = "admin@gmail.com",
+                Email = "admin@gmail.com",
+                EmailConfirmed = true
+            };
+
+            var userInDb = await userManager.FindByEmailAsync(admin.Email);
+
+            if (userInDb == null)
+            {
+                await userManager.CreateAsync(admin, "Admin@123");
+                await userManager.AddToRoleAsync(admin,Roles.Admin.ToString());
+            }
+        }
+    }
+}
