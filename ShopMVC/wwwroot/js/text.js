b6491a86921a97e1516e3ab03195ca0c34c998d1
fffvortex <fvortex@mail.ru>
2024-11-21 19:09:37 +0300

    added styles

---
+++ ShopMVC/wwwroot/js/text.js
@@ -0,0 +1,15 @@
+﻿
+var wrapper = document.querySelector(".typing");
+var text = document.querySelector(".typing .text");
+var textCont = text.textContent;
+text.style.display = "none";
+for (var i = 0; i < textCont.length; i++) {
+    (function (i) {
+        setTimeout(function () {
+            var texts = document.createTextNode(textCont[i])
+            var span = document.createElement('span');
+            span.appendChild(texts);
+            wrapper.appendChild(span);
+        }, 26 * i);
+    }(i));
+}
